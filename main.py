cc7e2fa9ec19b0e863fdc6a4765cdd75e3cee17e
Charu Thomas <charu@getox.com>
2025-02-03 14:51:00 -0800

    added objects

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import random
+
+class Node:
+    def __init__(self):
+        self.id = random.randint(1,40)
+
+class Env:
+    def __init__(self):
+        self.id = 0
+
+
 def create_env():
     """Instantiate an environment of nodes with coordinates at time t=0.
+
+    Returns::
+    <Env>
     """
 
-def move(x, y):
-    """Move a particular node to x, y from current location.
+def move(node, x, y):
+    """Move a particular node to: (x, y) from: current location.
+
+    Parameters::
+    node : <Node>
+    x : int
+    y : int
+
+    Returns::
+    <Null>
     """
 
 def main():
