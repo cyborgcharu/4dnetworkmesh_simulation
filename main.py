51e249c09648e9ddf32d221a40132920a94a9788
Charu Thomas <charu@getox.com>
2025-02-03 14:45:18 -0800

    added basic functions

---
+++ main.py
@@ -0,0 +1,15 @@
+def create_env():
+    """Instantiate an environment of nodes with coordinates at time t=0.
+    """
+
+def move(x, y):
+    """Move a particular node to x, y from current location.
+    """
+
+def main():
+    """
+    """
+    print("simulation started... t=0")
+
+if __name__ == "__main__":
+    main()
