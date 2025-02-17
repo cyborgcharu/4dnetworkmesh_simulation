601586a6725d56694dc29163695412e45d9a2bb6
Charu Thomas <charu@getox.com>
2025-02-17 09:00:47 -0800

    chore: add version mgmt

---
+++ __init__.py
@@ -0,0 +1,3 @@
+"""4D Network Mesh Simulation"""
+
+__version__ = '3.2.2'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="4dnetworkmesh",
+    version='3.2.2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'networkx>=3.0',
+        'matplotlib>=3.5.0',
+    ],
+)
